75e87573dc3036549d2958f47c707a0d60ba277f
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-07-26 01:48:13 +0300

    Update fonts

--- config.h
+++ config.h
@@ -4,9 +4,24 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 
+#define FONTSIZE "13"
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"Liberation Mono:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"NotoColorEmoji:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"JoyPixels:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"FontAwesome:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"misc fixed:pixelsize="FONTSIZE":antialias=true:autohint=true;1",
+	"unifont:fontformat=truetype:pixelsize="FONTSIZE":antialias=false:antialias=true:autohint=true",
+	"siji:pixelsize="FONTSIZE":antialias=true:autohint=true;1",
+	"IPAexMincho:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"IPAPMincho:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"IPAMincho:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"IPAexGothic:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"IPAPGothic:pixelsize="FONTSIZE":antialias=true:autohint=true",
+	"IPAGothic:pixelsize="FONTSIZE":antialias=true:autohint=true",
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
